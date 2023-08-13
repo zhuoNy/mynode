42eb004f4009dfbe030eb1aa1405422824ab31f9
zhuoNy <coble@126.com>
2023-08-13 16:10:04 +0800

    add: 监听回调

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 let express = require('express')
 let app = express()
+const port = 3000;
 
 app.get('/', function (res, rep) {
 
-  rep.send('Hello world')
-})
+  rep.send('Hello world!')
+ })
 
-app.listen(3000)
+app.listen(port, () => {
+
+  console.log(`Example app listening on port ${port}`)
+})
 
